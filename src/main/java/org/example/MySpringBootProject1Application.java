b529feb45a9374236ca698c0e51383cd87cd7f86
Harsh Pachouri <harshpach@gmail.com>
2025-11-30 21:19:04 +0530

    Initial commit: Event-driven microservices with Kafka

---
+++ src/main/java/org/example/MySpringBootProject1Application.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootProject1Application {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(MySpringBootProject1Application.class, args);
+	}
+}
